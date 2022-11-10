eb40e21fab166d38bd6ec98b229ec4986477c2a9
awoisoak <awoisoak@gmail.com>
2022-11-10 22:28:38 +0900

    Fix bugs and give precise feedback to the user about  the unsync files

--- check_sync.py
+++ check_sync.py
@@ -1,10 +1,9 @@
 import os
 from colorama import Fore, Style
 
-"""Check Sync
-Script to easily see where files are not syncronized between two folders.
+""" Check Sync
+Script to easily see which files are not syncronized between two folders.
 (Google Drive syncronization with external drives sucks!)
-
 """
 def printSuccess(msg):
     print(Fore.GREEN + msg)
@@ -16,38 +15,65 @@ def printError(msg):
 
 
 # External hdd path with the files to backup
-local='[LOCAL_PATH]'
+local_root = r'[LOCAL_PATH]'
 
 # Cloud drive path
-cloud='[CLOUD_PATH]'
+cloud_root = '[CLOUD_PATH]'
 
 
-# Loop sorted list of all root folders in $local ignoring hidden files 
-for folder in sorted((f for f in os.listdir(local) if not f.startswith(".")), key=str.lower):
+# Loop sorted list of all root items in $local ignoring hidden files 
+# First will check the number of files, if they are the same it will assume everything is sync correxctly
+# If number of files do not match it will prompt the unsync files
+for item in sorted((f for f in os.listdir(local_root) if not f.startswith(".")), key=str.lower):
+    local_path = os.path.join(local_root, item)
+    cloud_path = os.path.join(cloud_root, item)
     # If files are included in root $local folder we check whether it exists in Cloud Drive 
-    if os.path.isfile(os.path.join(local, folder)):
-        if os.path.isfile(os.path.join(cloud, folder)):
-            print(folder, 'present in both local and cloud  \n')
-        else:
-             printError(f"Error:  {folder} not present in cloud !!!\n")        
+    if os.path.isfile(local_path):
+        if os.path.isfile(cloud_path): print(item, 'present in both local and cloud  \n')
+        else: printError(f"Error: root file {item} not present in cloud !!!\n")        
         continue
 
-    print('Folder: ', folder)
+    print('Folder: ', item)
 
     local_counter = 0
-    for dirpath, dirnames, filenames in os.walk(local + folder):
-        local_counter += len(filenames)
+    for dirpath, dirnames, filenames in os.walk(local_path):
+        filtered_files = [x for x in filenames if not x.startswith(".")]
+        local_counter += len(filtered_files)
 
     cloud_counter = 0
-    for dirpath, dirnames, filenames in os.walk(cloud + folder):
-        cloud_counter += len(filenames)
+    for dirpath, dirnames, filenames in os.walk(cloud_path):
+        filtered_files = [x for x in filenames if not x.startswith(".")]
+        cloud_counter += len(filtered_files)
 
     if local_counter == cloud_counter:
         printSuccess(f"Sync files: {local_counter} \n")
-    else:
-        printError(f"Error: Unsync files ==> Local: {local_counter}, Cloud:{cloud_counter} \n")    
+    else:  
+        printError(f"Unsync files ==> Local: {local_counter}, Cloud:{cloud_counter}")       
+        if cloud_counter > local_counter:
+             printError(f"Error! {cloud_counter - local_counter} more file(s) in cloud than in local (duplicated)? Check them below:")  
+        elif cloud_counter != 0: 
+            printError(f"Error! {local_counter - cloud_counter} files are missing in cloud folder. Check them below: ")  
+
+        for dirpath, dirnames, files in os.walk(local_root + item):    
+            # print(f'Subfolder: {dirpath}')
+            localfiles = [x for x in files if not x.startswith(".")]
+            subpath = dirpath.replace(local_root,'')
+            cloud_subpath = os.path.join(cloud_root, subpath)    
+
+            # If folder does not exist in cloud report it and continue
+            if os.path.exists(cloud_subpath) == False:
+                printError(f"Error! Folder does not exist in cloud: {subpath}")
+                continue
 
 
+            cloudfiles = [x for x in os.listdir(cloud_subpath) if os.path.isfile(os.path.join(cloud_subpath, x)) and not x.startswith(".")]
+            set_dif = set(localfiles).symmetric_difference(set(cloudfiles))
+            for x in list(set_dif): # The list could be empty (When the subpath might be correclty sync)
+                    printError(f'diff: {subpath + "/" + x}') 
+
+                
+
+            
 
 
 
